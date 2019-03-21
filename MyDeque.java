a95f09097e4f5ebc282cbe2c5f354a3b4a98fdd8
Brandon Chao <bchao1@stuy.edu>
2019-03-21 14:55:15 -0400

    basic and add/remove is weird

---
+++ MyDeque.java
@@ -0,0 +1,96 @@
+import java.util.Objects;
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    String ary = "{";
+    if (end < start) {
+      for (int i = start; i < data.length; i++ ) {
+        ary += data[i] + ", ";
+      }
+      for (int i = 0; i <= end; i++) {
+        ary += data[i] + ", ";
+      }
+    }
+    else {
+      for (int i = 0; i < data.length; i++) {
+        ary += data[i] + ", ";
+      }
+    }
+    ary = ary.substring(ary.length()-2);
+    ary+= "}";
+    return ary;
+  }
+
+  public void addFirst(E element){
+    if (size == 0) {
+      data[start] = element;
+    }
+    else if (start == 0){
+      //resize thingy? or shift over?
+    }
+    else {
+      data[start-1] = element;
+      start--;
+    }
+    size++;
+  }
+
+  public void addLast(E element){
+    if (size == 0) {
+      data[end] = element;
+
+    }
+
+    size++;
+  }
+
+  public int[] resize() {
+    int[] d = new int[2*data.length];
+    if (end < start) {
+      for (int i = start; i < data.length; i++) {
+        d[]
+      }
+    }
+  }
+  public E removeFirst(){
+    E a = data[start];
+    data[start] = null;
+    start++;
+    size--;
+    return a;
+  }
+  public E removeLast(){
+    E a = data[end];
+    data[end] = null;
+    end--;
+    size--;
+    return a;
+  }
+  public E getFirst(){
+    return data[start];
+  }
+  public E getLast(){
+    return data[end];
+  }
+}
