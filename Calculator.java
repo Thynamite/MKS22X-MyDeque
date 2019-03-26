5fa10f965ace1bacd99701513021bd063baef9e5
Brandon Chao <bchao1@stuy.edu>
2019-03-26 14:31:01 -0400

    adjusted calculator to be not inputing into an arraylist holding the operators but just the deque for the numbers. Also returns the resulting value instead of only a zero since it needs to return the calculated postfix expression. Also inefficient checking of operation

--- Calculator.java
+++ Calculator.java
@@ -7,28 +7,48 @@ public class Calculator{
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-      ArrayList<String> list = new ArrayList<String>();
+      //ArrayList<String> list = new ArrayList<String>();
       MyDeque<Double> stack = new MyDeque<Double>();
       Scanner string = new Scanner(s);
 
       while(string.hasNext()) {
-        list.add(string.next());
+        String dummy = string.next();
+        //System.out.println(dummy);
+        if (checkOp(dummy)) {
+          Double first = stack.removeFirst();
+          Double second = stack.removeFirst();
+          if (dummy.equals("-")) {
+            Double ans = second - first;
+            stack.addFirst(ans);
+          }
+          if (dummy.equals("/")) {
+            Double ans = second / first;
+            stack.addFirst(ans);
+          }
+          if (dummy.equals("%")) {
+            Double ans = second % first;
+            stack.addFirst(ans);
+          }
+          if (dummy.equals("*")) {
+            Double ans = first * second;
+            stack.addFirst(ans);
+          }
+          if (dummy.equals("+")) {
+            Double ans = first + second;
+            stack.addFirst(ans);
+          }
+        }
+        else {
+          stack.addFirst(Double.parseDouble(dummy));
+        }
+
       }
       /*
       for (int x = 0; x < list.size(); x++) {
         System.out.println(list.get(x));
       }
       */
-
-      for (int i = 0; i < list.size(); i ++) {
-        if (checkOp(list.get(i))) {
-
-        }
-        else {
-          stack.addFirst((Double)list.get(i))
-        }
-      }
-      return 0;
+      return stack.getFirst();
     }
 
     public static boolean checkOp(String token) {
